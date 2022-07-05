e7816c2a41641c0d772a84359bad111492cf92eb
-Unknown- <mrunknownyt@gmail.com>
2022-07-04 18:48:04 -0600

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module ulibs.common {
+	exports main.java.ulibs.common.math;
+	exports main.java.ulibs.common.helpers;
+	exports main.java.ulibs.common.utils;
+	exports main.java.ulibs.common.utils.exceptions;
+	
+	requires transitive java.desktop;
+}
